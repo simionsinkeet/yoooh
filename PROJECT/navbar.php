76411a07ddd195a5ebe849bc8886a0b7eb3092fd
simionsinkeet <161486599+simionsinkeet@users.noreply.github.com>
2024-07-11 20:58:18 +0300

    commit01

---
+++ PROJECT/navbar.php
@@ -0,0 +1,15 @@
+<div id="header">
+    <div class="navbar">
+        <nav>
+            <h1 class="logo">RESTORAN</h1>
+            <ul>
+                <li><a href="#">HOME</a></li>
+                <li><a href="ABOUTUS.HTML">ABOUT US</a></li>
+                <li><a href="PRODUCTS.HTML">PRODUCTS</a></li>
+                <li><a href="signup.html">ORDER</a></li>
+            </ul>
+<div class="form">
+    <input type="text"placeholder="Search...">
+    <button type="submit">GO!</button>
+</div>
+</nav>
